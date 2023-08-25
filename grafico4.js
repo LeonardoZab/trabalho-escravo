f04b38226a33cd10805f95933e177e47bbd3e816
Leonardo Lorenz Zabel <leonardozalorenz@gmail.com>
2023-08-25 07:57:20 -0300

    Add files via upload

--- grafico4.js
+++ grafico4.js
@@ -29,3 +29,15 @@ const config = {
 }
 
 const graph = new Chart(ctx, config)
+
+const menu = document.querySelector('.menu')
+const side_bar = document.querySelector('.side-bar')
+const c = document.querySelector('.c')
+
+menu.addEventListener('click',()=>{
+    side_bar.style.left = '0'
+    side_bar.style.top = '10vh'
+})
+c.addEventListener('click',()=>{
+    side_bar.style.left = '-200vw'
+})
